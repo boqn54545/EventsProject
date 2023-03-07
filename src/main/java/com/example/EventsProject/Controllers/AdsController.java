b20991e46c9b65fad110e2521198b2d74ea05559
boqn54545 <Boqn54545@gmail.com>
2023-03-07 19:28:09 +0200

    Commit

--- src/main/java/com/example/EventsProject/Controllers/AdsController.java
+++ src/main/java/com/example/EventsProject/Controllers/AdsController.java
@@ -5,8 +5,13 @@ import com.example.EventsProject.Repositories.AdsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -21,5 +26,21 @@ public class AdsController {
         m.addAttribute("ads", ads);
         return "index";
     }
+    @GetMapping("createAds")
+    public String createAd(Model m){
+        Ad ad = new Ad();
+        m.addAttribute("ad",ad);
+        return "/createAds";
+    }
+    @PostMapping("submit")
+    private ModelAndView saveHotel(@Valid Ad ad, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return new ModelAndView("/createAd");
+        }else{
+            adsRepository.save(ad);
+            return new ModelAndView("redirect:/event");
+        }
+    }
+
 }
 
